3ba178196b6a638ead040de564a8b33205821a32
Pedro Castro <pedrocastro94@gmail.com>
2018-02-20 17:23:16 -0300

    questionario changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { HomePage } from '../pages/home/home';
 import { MissaoPage } from '../pages/missao/missao';
+import { QuestionarioPage } from '../pages/questionario/questionario';
 @Component({
   templateUrl: 'app.html'
 })
