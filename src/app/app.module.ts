23334060c1dc3b212c6a317d7955373a3dbfef4b
Fernando Sakabe <fernandokenjisakabe@gmail.com>
2018-02-20 15:16:24 -0300

    adicionei a pagina missnao

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { MissaoPageModule } from '../pages/missao/missao.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    MissaoPageModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
