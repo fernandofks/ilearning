b3f609428b7866b3d13b956267ea42c773bfb0d4
Pedro Castro <pedrocastro94@gmail.com>
2018-02-20 15:38:34 -0300

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,8 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+
+
 @Component({
   templateUrl: 'app.html'
 })
@@ -15,6 +17,7 @@ export class MyApp {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
+      splashScreen.show();
       splashScreen.hide();
     });
   }
